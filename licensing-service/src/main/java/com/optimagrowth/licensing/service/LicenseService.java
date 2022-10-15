c6dd9ba8f0391092d44f0a284e737586a5183f1c
erving <erving.kim@snowcorp.com>
2022-10-16 00:19:00 +0900

    feature: Locale에 따른 메시지 분기 작업

--- licensing-service/src/main/java/com/optimagrowth/licensing/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/licensing/service/LicenseService.java
@@ -1,12 +1,20 @@
 package com.optimagrowth.licensing.service;
 
 import com.optimagrowth.licensing.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
 public class LicenseService {
+
+    @Autowired
+    MessageSource messageSource;
+
     public License getLicense(String licenseId, String organizationId) {
         License license = new License();
         license.setId(new Random().nextInt(1000));
@@ -18,20 +26,20 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
-        if (license != null) {
+        if (!StringUtils.isEmpty(license)) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the post and the object is %s", license.toString());
+            responseMessage = String.format(messageSource.getMessage("license.create.message", null, locale), license.toString());
         }
         return responseMessage;
     }
 
     public String updateLicense(License license, String organizationId) {
         String responseMessage = null;
-        if (license != null) {
+        if (!StringUtils.isEmpty(license)) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the put and the object is %s", license.toString());
+            responseMessage = String.format(messageSource.getMessage("license.update.message", null, null), license.toString());
         }
         return responseMessage;
     }
